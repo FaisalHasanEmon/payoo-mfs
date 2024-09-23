3986f972ac506f1504f1b736d8d78dbde5615bb0
FaisalHasanEmon <faisalhasan5833@gmail.com>
2024-09-23 18:53:04 +0600

    Login page updated and home page added

--- js/login.js
+++ js/login.js
@@ -9,9 +9,17 @@ document
     // step-2: prevent default behavior (prevent reloading the browser)
     event.preventDefault(); // <-----Vejal to begginer
 
-    console.log("Button Clicked");
-
-    // step-3: get the phone number
+    // step-3: get the phone number and pi number
     const phoneNumber = document.getElementById("phone-number").value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById("pin-number").value;
+
+    // step-4: validate the phone number and pin number
+    // This is temporary. It is not good practice
+    if (phoneNumber === "5" && pinNumber === "1234") {
+      //   alert("Login Successfull");
+      //   step-5: Allow user to use the website
+      window.location.href = "/home.html";
+    } else {
+      alert("Incorrect pin and phone number");
+    }
   });
